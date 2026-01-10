b5c9063f9da8810af2c58cf0d9d247f73e4f8e36
rendiero25 <workspace.rendy@gmail.com>
2026-01-11 00:43:25 +0700

    vercel db connection

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || '';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || (import.meta.env.DEV ? 'http://localhost:5000' : '');
 
 const api = axios.create({
   baseURL: `${BACKEND_URL}/api`,
